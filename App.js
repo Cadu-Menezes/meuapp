23405f0643ffab0c153bb1d2d32c702cc7ae725b
Cadu-Menezes <cadumenezes051@gmail.com>
2023-05-03 15:46:59 -0300

    biscoito da sorte

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from "react";
+import { View, Text, Image, StyleSheet, TextInput, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+
+      frase: '',
+      img: require('./img/biscoito.png')
+    };
+    
+    this.frases = ['O cadu é foda!', 'O Yuri é foda!', 'O Vitor é foda!', 'O matheus é foda!', 'O Alex é foda!', 'O Wiliam é foda!']
+
+    this.quebrarbiscoito = this.quebrarbiscoito.bind(this);
+
+  }
+  
+  quebrarbiscoito(){
+    let sortearNum = Math.floor(Math.random() * this.frases.length)
+
+    this.setState({frase: '" '+ this.frases[sortearNum] + ' "',
+      img: require('./img/biscoitoAberto.png')              
+    })
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+
+          <Image 
+            source={this.state.img}
+            style={styles.img}  
+          />
+          
+          <Text style={styles.Texto}>{this.state.frase}</Text>
+
+          <Button 
+            title="Quebrar Biscoito"
+            onPress={this.quebrarbiscoito}
+          />
+
+      </View>
+    );
+  }
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
+
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+
+  input:{
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 20,
+    fontSize: 20,
+    padding: 5,
+  },
+
+  Texto:{
+
+    color: '#dd7b22',
+    textAlign: 'center',
+    fontSize: 25,
+    marginBottom: 50,
+    marginTop: 50,
+    fontStyle: 'italic'
+  },
+
+  img:{
+    width: 300,
+    height: 300,
+    textAlign: 'center'
+  },
+
+
 });
